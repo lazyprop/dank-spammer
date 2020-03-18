a5880afa881da060447b88d9eed71b07fc841678
Ishaan Verma <ishaan56verma@gmail.com>
2020-03-18 21:04:13 +0530

    Removed cyclic import, changed trigger to "//wtf//", added use() and sell()

--- main.py
+++ main.py
@@ -14,37 +14,22 @@ SENPAI_ID = info["userid"]
 
 client = discord.Client()
 
-def checkstatus(name):
-    stringdata = open("status.json","r").read()
-    jsondata = json.loads(str(stringdata))
-    # print(stringdata)
-    return jsondata["services"][name]
-    # return
-
-def flipstatus(name):
-    jsondata = json.loads(open("status.json","r").read())
-    jsondata["services"][name]^= 1
-
-    file = open("status.json","w")
-    file.write(json.dumps(jsondata))
-    file.close()
-
 @client.event
 async def on_message(message):
     splitmsg = message.content.split()
     # print(splitmsg)
 
-    if (message.content.startswith("wtf")):
+    if (message.content.startswith("//wtf// ")):
         if (str(message.author) == SENPAI):
 
             if (splitmsg[1] == "start"):
             
                 if (splitmsg[2] == "ezmoney"):
-                    await send_message(message,"wtf start beg")
+                    await send_message(message,"//wtf// start beg")
                     await asyncio.sleep(3)
-                    await send_message(message,"wtf start postmeme")
+                    await send_message(message,"//wtf// start postmeme")
                     await asyncio.sleep(3)
-                    await send_message(message,"wtf start deposit")
+                    await send_message(message,"//wtf// start deposit")
 
                 if (splitmsg[2] == "beg"):
                     delay = 30
@@ -65,20 +50,16 @@ async def on_message(message):
                     await postmeme(message, delay)
 
                 if (splitmsg[2] == "gamble"):
-                    delay = 3
-                    if (len(splitmsg) > 3):
-                        delay = int(splitmsg[3])
-                        await postmeme(message,delay)
-
-                    amount = int(splitmsg[3])
-                    tries = int(splitmsg[4])
                     await gamble(message,amount,tries)
 
                 if (splitmsg[2] == "clear"):
                     await clear(message,12,60)
 
                 if (splitmsg[2] == "sell"):
-                    await sellUseless(message)
+                    await sell(message)
+
+                if (splitmsg[2] == "use"):
+                    await use(message)
 
             if (splitmsg[1] == "stop"):
                 if (len(splitmsg) <= 2):
@@ -94,12 +75,6 @@ async def on_message(message):
                 message.author.id,SENPAI_ID
             )) 
     
-async def send_message(message,content,clear=False):
-    await message.channel.send(content)
-    if (clear):
-        await message.channel.send("pls clear")
-
-
 def main():
     
     jsondata = json.loads(open("status.json","r").read())
--- services.py
+++ services.py
@@ -1,5 +1,5 @@
 import asyncio
-from main import checkstatus, flipstatus, send_message
+import json
 
 async def beg(message,delay = 30):
     if (checkstatus("beg")):
@@ -39,12 +39,17 @@ async def postmeme(message, delay = 60):
             await send_message(message,x)
             await asyncio.sleep(delay)
 
-async def gamble(message,amount, tries,delay = 3):
+async def gamble(message,delay = 3):
     if (checkstatus("gamble")):
         await send_message(message,"[.] Already gambling.")
         return
 
     flipstatus("gamble")
+
+    args = message.content[3:].split()
+    amount = args[0].split(":")[0]
+
+
     print("[.] Gambling with {} coins a total of {} times...".format(amount, tries))
     await send_message(message,"[.] Gambling with {} coins a total of {} times...".format(amount, tries))
     count = 0
@@ -70,39 +75,75 @@ async def clear(message,num,delay):
         await send_message(message,"pls clear {}".format(num))
         await asyncio.sleep(delay)
 
-async def sellUseless(message):
+async def sell(message,delay=4):
     if (checkstatus("sell")):
-        await send_message("message,Already selling.")
-        return
-        
-    bread = 15
-    candy = 12
-    chillpill = 11
-    cookie = 9
-
+        await send_message(message,"[.] Already selling.")
+        return        
+    
     flipstatus("sell")
-    print("[.] Selling all useless items...")
-    await send_message(message,"[.] Selling all useless items...")
+    print("[.] Selling...")
+    await send_message(message,"[.] Selling...")
 
-    while bread:
-        await send_message(message,"pls sell bread")
-        await asyncio.sleep(3)
-        bread-=1
+    for item in message.content.split()[3:]:
+        it = item.split(':')[0]
+        val = int(item.split(':')[1])
+
+        await send_message(message,"[>] Selling {} {}s...".format(val,it))
+
+        count = 0
+        while count < val:
+            await send_message(message, "pls sell {}".format(it))
+            count+= 1
+            await asyncio.sleep(delay)
+
+        await send_message(message, "[>] Sold {} {}s.".format(val,it))
+
+    print("[.] All items sold!")
+    await send_message(message,"[.] All items sold!")
+
+async def use(message,delay=5):
+    if (checkstatus("use")):
+        await send_message(message,"[.] Already using.")
+        return        
     
-    while candy:
-        await send_message(message,"pls sell candy")
-        await asyncio.sleep(3)
-        candy-=1
-    
-    while chillpill:
-        await send_message(message,"pls sell chillpill")
-        await asyncio.sleep(3)
-        chillpill-=1
+    flipstatus("use")
 
-    while cookie:
-        await send_message(message,"pls sell cookie")
-        await asyncio.sleep(3)
-        cookie-=1
+    print("[.] Using...")
+    await send_message(message,"[.] Using...")
 
-    await send_message(message,"pls deposit all")
-    print("[.] All useless items sold!")
+    for item in message.content.split()[3:]:
+        it = item.split(':')[0]
+        val = int(item.split(':')[1])
+
+        await send_message(message,"[>] Using {} {} times...".format(it,val))
+
+        count = 0
+        while count < val:
+            await send_message(message, "pls use {}".format(it))
+            count+= 1
+            await asyncio.sleep(delay)
+
+        await send_message(message, "[>] Used {} {} times.".format(it,val))
+
+    print("[.] All items used!")
+    await send_message(message,"[.] All items used!")
+
+async def send_message(message,content,clear=False):
+    await message.channel.send(content)
+    if (clear):
+        await message.channel.send("pls clear")
+
+def checkstatus(name):
+    stringdata = open("status.json","r").read()
+    jsondata = json.loads(str(stringdata))
+    # print(stringdata)
+    return jsondata["services"][name]
+    # return
+
+def flipstatus(name):
+    jsondata = json.loads(open("status.json","r").read())
+    jsondata["services"][name]^= 1
+
+    file = open("status.json","w")
+    file.write(json.dumps(jsondata))
+    file.close()
